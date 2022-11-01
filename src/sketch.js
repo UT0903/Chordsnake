8c0ed6891dba50ff570b2a9b43aacf517bfe6fdc
陳柏衡 <b07902114@ntu.edu.tw>
2022-11-01 17:47:27 +0800

    Add snake template form internet

---
+++ src/sketch.js
@@ -0,0 +1,71 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/AaGK-fj-BAM
+
+var s;
+var scl = 40;
+var food;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+  
+}
+
+function pickLocation() {
+  var cols = floor(width / scl);
+  var rows = floor(height / scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
+}
+
+function draw() {
+  // background("#8ecc39");
+  // noStroke()
+  stroke("#8ecc39");
+  for (let i = 0; i < width; i = i + scl){
+    for (let j = 0; j < height; j = j + scl){
+      if ((i/scl+j/scl) % 2) {
+            fill("#8ecc39");
+            rect(i, j, scl, scl);
+          }
+      else {
+            fill("#a2d645");
+            rect(i, j, scl, scl);
+          }
+    }
+  }
+  if (s.eat(food)) {
+    pickLocation();
+  }
+  s.death();
+  s.update();
+  s.show();
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+
+
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
