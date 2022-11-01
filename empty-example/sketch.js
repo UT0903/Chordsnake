6c491cad08055408c5e8731ca23a60bad4996b77
UT0903 <b07902048@ntu.edu.tw>
2022-11-01 17:40:29 +0800

    add how to exec readme

---
+++ empty-example/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+  fill("blue");
+  createCanvas(600, 400);
+}
+
+function draw() {
+  // put drawing code here
+  text("hello world", 20, 20);
+  fill("yellow");
+  rect(100, 100, 20, 20);
+}
